511dc8a6c39ceb1116b8a4a87a349ccc04fcfbf4
Glitched Da Cat <143963110+glitchedtahcat@users.noreply.github.com>
2024-05-17 09:48:29 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,14 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in Html/Javascript these days.
-    I also sometimes code python but i stuck at this sadly
-    Here is my <a href="https://glitched-studios.vercel.app/" target="_blank">Website</a>!`,
-  "cs-degree": `This is my CS degree. Haha u got trolled i dont have one`,
+  pc: `This is my PC. I work mostly in HTML/Javascript these days.
+    I also sometimes code python but I stuck at this sadly
+    Here is my <a href="coming soon" target="_blank">Website</a>!`,
+  "cs-degree": `This is my CS degree. Haha, you got trolled I don't have one`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
-  tv: `That's my TV. I've been watching youtube and anime ( and porn <strong>JK</strong>`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is My Non Existing Resume which when i make one i will add it here`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `This Is were i store my languge arts book i have stolen from my school (jk i didnt do that) and this is where spiders live`,
+  tv: `That's my TV. I've been watching YouTube and anime`,
+  bed: `This is where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is My Non-Existing Resume which when I make one I will add it here`,
+  projects: `My Projects Arent Here Rn`,
+  library: `This Is were I store my language arts book I have stolen from my school (jk I didn't do that) and this is where spiders live`,
 };
